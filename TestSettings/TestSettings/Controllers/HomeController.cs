1a6861e1ff0cf433a7321f93e80f13b7f1170a39
Meglena Novoselska <meglena.novoselska@gmail.com>
2016-02-17 16:58:45 +0000

    wip

--- TestSettings/TestSettings/Controllers/HomeController.cs
+++ TestSettings/TestSettings/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestSettings.Models;
 
 namespace TestSettings.Controllers
 {
@@ -12,7 +14,27 @@ namespace TestSettings.Controllers
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-            return View();
+            var model = new PassedString();
+            
+            using (var conn = new SqlConnection(Properties.Settings.Default.DbConnectionString))
+            {
+                var command = new SqlCommand();
+                conn.Open();
+                command.Connection = conn;
+                command.CommandText = "select top 1 * from Customer";
+                command.CommandType = System.Data.CommandType.Text;
+                var reader = command.ExecuteReader();
+
+                if(reader.Read())
+                {
+                    model.text = reader.GetString(reader.GetOrdinal("Email"));
+                }
+
+                
+                conn.Close();
+            }
+           
+            return View(model);
         }
 
         public ActionResult About()
---
+++ TestSettings/TestSettings/Models/PassedString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestSettings.Models
+{
+    public class PassedString
+    {
+        public virtual string text { get; set; }
+
+    }
+}
