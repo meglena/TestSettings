1f3d163c94bb599916bfc38bb761cdd83e1bb315
Meglena Novoselska <meglena.novoselska@gmail.com>
2016-02-17 17:08:42 +0000

    wip

---
+++ TestSettings/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestSettings.Models;
+
+namespace TestSettings.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            var model = new PassedString();
+            
+            using (var conn = new SqlConnection(Properties.Settings.Default.DbConnectionString))
+            {
+                var command = new SqlCommand();
+                conn.Open();
+                command.Connection = conn;
+                command.CommandText = "select top 1 * from Customer";
+                command.CommandType = System.Data.CommandType.Text;
+                var reader = command.ExecuteReader();
+
+                if(reader.Read())
+                {
+                    model.text = reader.GetString(reader.GetOrdinal("Email"));
+                }
+
+                
+                conn.Close();
+            }
+           
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
--- TestSettings/TestSettings/Controllers/HomeController.cs
+++ TestSettings/TestSettings/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using TestSettings.Models;
 
 namespace TestSettings.Controllers
 {
@@ -14,27 +12,7 @@ namespace TestSettings.Controllers
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-            var model = new PassedString();
-            
-            using (var conn = new SqlConnection(Properties.Settings.Default.DbConnectionString))
-            {
-                var command = new SqlCommand();
-                conn.Open();
-                command.Connection = conn;
-                command.CommandText = "select top 1 * from Customer";
-                command.CommandType = System.Data.CommandType.Text;
-                var reader = command.ExecuteReader();
-
-                if(reader.Read())
-                {
-                    model.text = reader.GetString(reader.GetOrdinal("Email"));
-                }
-
-                
-                conn.Close();
-            }
-           
-            return View(model);
+            return View();
         }
 
         public ActionResult About()
